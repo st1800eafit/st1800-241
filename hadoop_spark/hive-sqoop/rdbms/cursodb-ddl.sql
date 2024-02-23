39fc9a32c08672a29940ef58ac21271b89daf216
edwinm67 <emontoya@eafit.edu.co>
2024-02-23 17:11:49 -0500

    first commit

--- hadoop_spark/hive-sqoop/rdbms/cursodb-ddl.sql
+++
@@ -1,8 +0,0 @@
-$ mysql -u admin -h localhost -p <pass>
-
-CREATE DATABASE cursodb;
-USE cursodb;
-CREATE TABLE `cursodb`.`employee` (  `emp_id` INT NOT NULL,  `name` VARCHAR(45),  `salary` INT,  PRIMARY KEY (`emp_id`));
-CREATE USER 'curso'@'%' IDENTIFIED BY 'curso';
-GRANT ALL PRIVILEGES ON cursodb.* TO 'curso'@'%';
-
